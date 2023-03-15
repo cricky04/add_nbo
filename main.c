2b9293f8334404b8a5042b97e3f87d119bee6966
Park Seonghyun <42666046+cricky04@users.noreply.github.com>
2023-03-15 16:41:35 +0900

    code

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    uint32_t a, b, c;
+    uint32_t t;
+    FILE * fone = fopen(argv[1], "r");
+    FILE * ftwo = fopen(argv[2], "r");
+
+    fread(&a, sizeof(uint32_t), 1, fone);
+    fread(&b, sizeof(uint32_t), 1, ftwo);
+
+    // ntoh
+    a = ((a & 0x000F) << 24) ^ ((a & 0x00F0) << 8) ^ ((a & 0x0F00) >> 8) ^ ((a & 0xF000) >>24);
+
+    b = ((b & 0x000F) << 24) ^ ((b & 0x00F0) << 8) ^ ((b & 0x0F00) >> 8) ^ ((b & 0xF000) >>24);
+
+    c = a + b;
+    printf("%u(0x%8x) + %u(0x%8x) = %u(0x%8x)", a, a, b, b,c, c);
+}
